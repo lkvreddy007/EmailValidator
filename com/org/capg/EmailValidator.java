3cc402b0d7ab87dc735477c3c65eac9501816e8f
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-28 18:55:20 +0530

    [Krishna]added-1stMandatoryPart

--- com/org/capg/EmailValidator.java
+++ com/org/capg/EmailValidator.java
@@ -1,7 +1,19 @@
 package com.org.capg;
 
-public class EmailValidator {
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class EmailValidator {	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Email Validator");
+		Pattern p= Pattern.compile("(^[a-zA-Z0-9]+)");
+		Matcher m=p.matcher("krishna");
+		boolean b=m.matches();
+		if(b) {
+			System.out.println("Matches");
+		}
+		else {
+			System.out.println("Doesn't Match");
+		}
 	}
 }
