f99dff64ea9cb78a5951bd54c9b2def4b019c343
lkvreddy007 <44741736+lkvreddy007@users.noreply.github.com>
2020-09-29 20:45:31 +0530

    Update EmailValidator.java

--- com/org/capg/EmailValidator.java
+++ com/org/capg/EmailValidator.java
@@ -7,7 +7,7 @@ public class EmailValidator {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Email Validator");
 
-		Pattern p= Pattern.compile("^[a-zA-Z0-9]+([.+-_][a-zA-Z]+)?@[a-zA-Z0-9]+\\.[a-z]{2,4}(\\.[a-z]{2})?$");
+		Pattern p= Pattern.compile("^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\\.[a-z]{2,4}(\\.[a-z]{2})?$");
 		Matcher m=p.matcher("abc_xyz@yahoo.com.in");
 		boolean b=m.matches();
 		if(b) {
